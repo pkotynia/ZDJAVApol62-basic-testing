27d241acf863785ed9a07a990cdc0cd2bd62224c
Kotynia <piotr.kotynia@harman.com>
2021-01-10 18:40:54 +0100

    task_01 to task_07

---
+++ src/main/java/exercise/task_03/ArrayExample.java
@@ -0,0 +1,16 @@
+package exercise.task_03;
+
+/**
+ * Wykorzystując podejście TDD zaimplementuj w klasie ArrayExample metodę:
+ *
+ * public String[] removeDuplicates(String[] array)
+ *
+ * Metoda powinna zwrócić nową tablicę bez duplikatów.
+ */
+
+public class ArrayExample {
+
+    public String[] removeDuplicates(String[] array) {
+        return null;
+    }
+}
---
+++ src/test/java/exercise/task_01/CalculatorTest.java
@@ -0,0 +1,13 @@
+package exercise.task_01;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void environmentTest() {
+
+    }
+
+
+}
